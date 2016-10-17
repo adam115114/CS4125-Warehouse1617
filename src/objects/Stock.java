845528d9a842885ba50a7918c4f0286b637616f3
davidsimsdoolin <dsims1000@gmail.com>
2016-10-17 17:55:14 +0100

    Stock object

--- src/objects/Stock.java
+++ src/objects/Stock.java
@@ -3,5 +3,35 @@ package objects;
 /**
  * Created by David Sims on 17/10/2016.
  */
-public class Stock {
+public class Stock
+{
+    private int stockNum;
+    private String name;
+    private int quantity;
+    private float price;
+
+    public Stock (int inNum , String inName, int inQuantity, float inPrice)
+    {
+        stockNum = inNum;
+        name = inName;
+        quantity = inQuantity;
+        price = inPrice;
+    }
+
+    public int getStockNum() {return stockNum;}
+
+    public String getName() {return name;}
+
+    public int getQuantity() {return quantity;}
+
+    public float getPrice() {return price;}
+
+
+    public void setStockNum(int inNum) {stockNum = inNum;}
+
+    public void setName(String inName) {name = inName;}
+
+    public void setQuantity(int inQuantity) {quantity = inQuantity;}
+
+    public void setPrice(float inPrice) {price = inPrice;}
 }
